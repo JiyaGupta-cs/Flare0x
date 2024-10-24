7a0c3f1f467887014ca55f00725853d187070fde
Jiya Gupta <coderaholic21@gmail.com>
2024-10-24 22:45:33 +0530

    analytics ui

--- src/containers/contract/ReadContract.jsx
+++ src/containers/contract/ReadContract.jsx
@@ -55,36 +55,35 @@ export function ReadContract() {
       ) : (
         <div className="relative overflow-x-auto shadow-md rounded-lg">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-              <tr>
-                <th scope="col" className="px-6 py-3 
-                                 bg-orange-600 relative z-10 bg-opacity-40 flex flex-col rounded-xl p-4 text-[#DA810D]
+            <thead className="text-xs text-gray-700 uppercase dark:text-gray-400  my-3">
+              <tr >
+                <th scope="col" className="px-6 py-7  m-1 my-3 text-center
+                                 bg-orange-600 relative  bg-opacity-40 flex flex-col rounded-xl p-4 text-[#DA810D]
 
                  dark:bg-gray-800">Task</th>
-                <th scope="col" className="px-6 py-3
-               bg-orange-600  z-10 bg-opacity-40  rounded-xl text-[#DA810D]
+                <th scope="col" className="px-6  py-7  m-1 my-3 text-center
+               bg-orange-600   bg-opacity-40  rounded-xl text-[#DA810D]
 
-                ">Duration (seconds)</th>
-                <th scope="col" className="px-6 py-3
-                                   bg-orange-600 relative z-10 bg-opacity-40 flex flex-col rounded-xl p-4 text-[#DA810D]
-                  dark:bg-gray-800">Completed</th>
+                ">Duration (s)</th>
+                <th scope="col" className="px-6  py-7  m-1 my-3 text-center
+                                   bg-orange-600 relative  bg-opacity-40 flex flex-col rounded-xl p-4 text-[#DA810D]
+                  dark:bg-gray-800">Stat</th>
               </tr>
             </thead>
             <tbody>
               {taskHistory?.map((task, index) => (
-                <tr key={index} className=" ">
-                  <th scope="row" className="px-6 py-4 font-medium  whitespace-nowrap
-                   bg-orange-600 relative z-10 bg-opacity-20 flex flex-col rounded-xl p-4 text-[#DA810D]
-                   dark:text-white dark:bg-gray-800">
+                <tr key={index} className="my-10">
+                  <th scope="row" className="px-6 py-4 m-1 font-medium  whitespace-nowrap
+                   bg-orange-600 relative  bg-opacity-20 flex flex-col rounded-xl p-4 text-[#DA810D]
+                   dark:text-white dark:bg-gray-800 text-center">
                     {task.label}
                   </th>
-                  <td className="px-6 py-4
-                                    bg-orange-600 relative z-10 bg-opacity-20  rounded-xl text-[#DA810D]
-
+                  <td className="px-6 py-4 text-center
+                                    bg-orange-600 relative m-1 bg-opacity-40  rounded-xl text-[#DA810D]
                   ">{task.duration}</td>
-                  <td className="px-6 py-4 
-                  bg-orange-600 relative z-10 bg-opacity-20 flex flex-col rounded-xl p-4 text-[#DA810D]
-                  dark:bg-gray-800">{task.completed ? "Yes" : "No"}</td>
+                  <td className="px-6 py-4  text-center
+                  bg-orange-600 relative  bg-opacity-20 m-1 flex flex-col rounded-xl p-4 text-[#DA810D]
+                  dark:bg-gray-800">{task.completed ? "Success" : "Fail"}</td>
                 </tr>
               ))}
             </tbody>
